b2cb4b2bd2c678dd3e2af9744a0f8217a323c11d
Prashant Dombale <prashant.dombale@gmail.com>
2021-06-19 22:03:48 +0530

    Day 2- commit added the queueing system for chests, timer based opening and gems calculation

--- Assets/Scripts/ChestSlot.cs
+++ Assets/Scripts/ChestSlot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,18 +7,68 @@ using UnityEngine.UI;
 
 public class ChestSlot : MonoBehaviour
 {
-    public bool equipped;
+    public bool equipped,started;
     public Button skipTime;
-    public TextMeshProUGUI timer;
+    public TextMeshProUGUI timerText;
     public TextMeshProUGUI skipGems;
+    public float timer;
+    float hours;
+    float minutes;
 
     //Rewards
     public int GoldReward;
     public int GemReward;
     private void Awake()
     {
-        equipped = false;
+        equipped = started = false;
         skipGems.enabled = false;
-        timer.enabled = false;
+        timerText.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (equipped && started)
+        {
+            float hours = timer / 3600;
+            float minutes = timer / 60;
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                timerText.text = Mathf.FloorToInt(hours) + "h" + Mathf.FloorToInt(minutes / Mathf.CeilToInt(hours)) + "m" + Mathf.FloorToInt(timer % 60) + "s";
+                skipGems.text = "GEMS: " + Mathf.CeilToInt(minutes / 10);
+            }
+            else if (timer < 0) {
+                timer = 0;
+                UnEquipSlot();
+                ChestSpawner.FreeSlot();
+            }
+        }
+        else if (equipped && !started)
+        {
+            float hours = timer / 3600;
+            float minutes = timer / 60;
+            if (Mathf.FloorToInt(hours) != 0)
+                timerText.text = "Locked: " + hours + "h";
+            else timerText.text = "Locked " + minutes + "m";
+            skipGems.text = "";
+        }
+        
+    }
+
+    private void UnEquipSlot()
+    {
+        //add coins and gems and free the slot
+        skipTime.image.color = Color.grey;
+        timerText.text = "";
+        skipGems.text = "";
+        equipped = false;
+    }
+
+    public void enableText() {
+        skipGems.enabled = true;
+        timerText.enabled = true;
+    }
+    public void StartOpening() {
+        started = true;
     }
 }
--- Assets/Scripts/ChestSpawner.cs
+++ Assets/Scripts/ChestSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,14 +7,19 @@ public class ChestSpawner : MonoBehaviour
 {
     public Chest[] chests;
     public Button spawnButton;
-    public GameObject[] chestSlot;
-    private int empty;
+    public ChestSlot[] chestSlot;
+    private static int empty;
+    private Queue<ChestSlot> slotQueue;
+    private static bool isProcessing, isEmpty;
+
 
     // Start is called before the first frame update
     private void Awake()
     {
-        chestSlot = new GameObject[4];
+        slotQueue = new Queue<ChestSlot>();
         empty = 0;
+        isProcessing  = false;
+        isEmpty = true;
     }
     void Start()
     {
@@ -25,13 +31,46 @@ public class ChestSpawner : MonoBehaviour
         Debug.Log("Button clicked");
         int spawnNumber = UnityEngine.Random.Range(0,chests.Length);
         Chest newChest = chests[spawnNumber];
-        //begin here tomorrow
+        ChestSlot slot;
+        if (empty < chestSlot.Length)
+        {
+            slotQueue.Enqueue(chestSlot[empty]);
+            isEmpty = false;
+            slot = chestSlot[findEmpty()];
+            slot.enableText();
+            slot.equipped = true;
+            slot.GemReward = UnityEngine.Random.Range(newChest.gemMin, newChest.gemMax + 1);
+            slot.GoldReward = UnityEngine.Random.Range(newChest.coinsMin, newChest.coinsMax + 1);
+            slot.skipTime.image.color = newChest.color;
+            slot.timer = newChest.Timer * 60;
+            empty++;
+        }
+        else {
+            Debug.Log("All slots full wait for some time bro");
+        }
         
+    }
+
+    private int findEmpty()
+    {
+        for (int i = 0; i < chestSlot.Length;i++) {
+            if (chestSlot[i].equipped == false) return i;
+        }
+        return -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isProcessing && !isEmpty) {
+            ChestSlot slot = slotQueue.Dequeue().GetComponent<ChestSlot>();
+            slot.StartOpening();
+            isProcessing = true;
+        }
+    }
+
+    public static void FreeSlot() {
+        ChestSpawner.isProcessing = false;
+        ChestSpawner.empty--;
     }
 }
